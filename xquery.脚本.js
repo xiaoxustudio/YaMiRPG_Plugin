50a5522bc640d29c4e30ae82d0e55554ee48939e
xiaoxustudio <1783558957@qq.com>
2023-11-16 17:27:48 +0800

    优化操作，增加nth方法选择子元素，on、off方法监听事件

--- xquery.脚本.js
+++ xquery.脚本.js
@@ -1,6 +1,6 @@
 /*
  * @Author: xuranXYS
- * @LastEditTime: 2023-11-15 20:59:14
+ * @LastEditTime: 2023-11-16 16:25:49
  * @GitHub: www.github.com/xiaoxustudio
  * @WebSite: www.xiaoxustudio.top
  * @Description: By xuranXYS
@@ -9,8 +9,8 @@
 @plugin Xquery
 @version 1.0
 @author 徐然
-@link
-@desc
+@link https://space.bilibili.com/291565199
+@desc 
 
 可快速选择UI
 
@@ -30,7 +30,8 @@ root: RootElement,
 目前支持的属性：
 名称: "name"、内容: "content"、
 颜色: "color"、大小: "size"、
-垂直居中: "verticalAlign"、水平居中: "horizontalAlign"、
+垂直居中: "verticalAlign" 支持字符串：top、middle、bottom
+水平居中: "horizontalAlign" 支持字符串：left、center、right
 图片: "image"、行距: "lineSpacing"、
 字体: "font"、边框: "border"、
 ID: "ID"、方向: "direction"、
@@ -183,6 +184,37 @@ xQuery.fn = {
     }
     return arr_obj
   },
+  parseTextChild(text) {
+    let words = text.split("n")
+    if (words.length <= 0 || !(/[n]/.test(text)) || !(/([0-9]+)?n([0-9]+)?/.test(text))) { return [] }
+    this.elemMap = xQuery.fn.map()
+    let left = 0
+    if (/[0-9]+/.test(words[0])) {
+      left = new Function("return " + words[0])()
+    }
+    let right = 0
+    if (/[0-9]+/.test(words[1])) {
+      right = new Function("return " + words[1])()
+    }
+    let num = xQuery.fn.getObjNum(this)
+    let index;
+    let arr = []
+    for (index = 0; index < num.length; index++) {
+      if (left == 0 && right == 0) {
+        arr.push(this[index])
+      } else if (index % left === 0) {
+        if (right !== 0) {
+          let sub_index = parseInt(index + right)
+          if (this?.[sub_index]) {
+            arr.push(this?.[sub_index])
+          }
+        } else {
+          arr.push(this?.[index])
+        }
+      }
+    }
+    return arr
+  },
   map(a = false) {
     if (a) {
       return {
@@ -230,6 +262,7 @@ xQuery.fn = {
         direction: "方向",
         display: "显示",
         hoverImage: "悬停图片",
+        align: "对齐",
       }
     }
     return {
@@ -247,6 +280,7 @@ xQuery.fn = {
       方向: "direction",
       显示: "display",
       悬停图片: "hoverImage",
+      对齐: "align",
     }
   },
   merge: function (array1, array2) {
@@ -288,18 +322,212 @@ xQuery.fn = {
   getObjNum(t) {
     return Object.keys(t).filter(index => !isNaN(index))
   },
-  text(content) {
+  _UIPrototype(prototype, content, call_back = () => { }, scall_back = () => { }) {
+    const to_call = (con) => {
+      let res = call_back(con)
+      if (res) { return res } else { return con }
+    }
+    content = to_call(content)
     let num = xQuery.fn.getObjNum(this)
     let elemMap = xQuery.fn.map(true)
     if (content) {
       for (let i in num) {
         let na = this[i].constructor.name
         if (na in elemMap) {
-          this[i].content = content
+          this[i][prototype] = content
+        }
+      }
+    } else {
+      let str = ""
+      for (let i in num) {
+        let na = this[i].constructor.name
+        if (na in elemMap) {
+          if (typeof this[i][prototype] === "string") {
+            str += this[i][prototype]
+          } else if (typeof this[i][prototype] == "number") {
+            if (!(str instanceof Array)) { if (str.length > 0) { str = [str] } else { str = [] } }
+            str.push(this[i][prototype])
+          } else {
+            str += this[i][prototype]
+          }
+        }
+      }
+      return str
+    }
+  },
+  _UIFunction(prototype, args, call_back = () => { }) {
+  },
+  text(content) {
+    return this._UIPrototype("content", content)
+  },
+  name(content) {
+    return this._UIPrototype("name", content)
+  },
+  isHexColor(color) {
+    // 判断是否以'#'开头
+    if (!color.startsWith('#')) return false;
+
+    // 使用正则表达式验证六位十六进制数字
+    const pattern = /^#([a-fA-F0-9]{6})$/;
+    const pattern1 = /^#([a-fA-F0-9]{3})$/;
+    const pattern2 = /^#([a-fA-F0-9]{4})$/;
+    const pattern3 = /^#([a-fA-F0-9]{8})$/;
+    return pattern.test(color) ? true : pattern1.test(color) ? true : pattern2.test(color) ? true : pattern3.test(color) ? true : false;
+  },
+  HexToRgba(hex) {
+    if (/^#([0-9a-fA-F]{3})$/.test(hex)) {
+      const r = (hex[1] + hex[1]).toString(16)
+      const g = (hex[2] + hex[2]).toString(16)
+      const b = (hex[3] + hex[3]).toString(16)
+      return r + g + b + "FF"
+    }
+    if (/^#([0-9a-fA-F]{4})$/.test(hex)) {
+      const r = (hex[1] + hex[1]).toString(16)
+      const g = (hex[2] + hex[2]).toString(16)
+      const b = (hex[3] + hex[3]).toString(16)
+      const a = (hex[4] + hex[4]).toString(16)
+      return r + g + b + a
+    }
+    if (/^#([0-9a-fA-F]{6})$/.test(hex)) {
+      const r = (hex[1] + hex[2]).toString(16)
+      const g = (hex[3] + hex[4]).toString(16)
+      const b = (hex[5] + hex[6]).toString(16)
+      return r + g + b
+    }
+    if (/^#([0-9a-fA-F]{8})$/.test(hex)) {
+      const r = (hex[1] + hex[2]).toString(16)
+      const g = (hex[3] + hex[4]).toString(16)
+      const b = (hex[5] + hex[6]).toString(16)
+      const a = (hex[7] + hex[8]).toString(16)
+      return r + g + b + a
+    }
+    throw new Error("不能解析颜色");
+  },
+  color(content) {
+    return this._UIPrototype("color", content, (con) => {
+      if (con) {
+        return xQuery.fn.isHexColor(content) ? xQuery.fn.HexToRgba(content) : new Error("不能解析颜色");
+      }
+    })
+  },
+  size(content) {
+    return this._UIPrototype("size", content)
+  },
+  lineSpacing(content) {
+    return this._UIPrototype("lineSpacing", content)
+  },
+  font(content) {
+    return this._UIPrototype("font", content)
+  },
+  typeface(content) {
+    return this._UIPrototype("typeface", content)
+  },
+  effect(content) {
+    return this._UIPrototype("effect", content)
+  },
+  overflow(content) {
+    return this._UIPrototype("overflow", content)
+  },
+  valign(content) {
+    return this._UIPrototype("verticalAlign", content)
+  },
+  halign(content) {
+    return this._UIPrototype("horizontalAlign", content)
+  },
+  direction(content) {
+    return this._UIPrototype("direction", content)
+  },
+  width() {
+    return this._UIPrototype("width").join(" ")
+  },
+  height() {
+    return this._UIPrototype("height").join(" ")
+  },
+  align() {
+    return this._UIPrototype("align")
+  },
+  nth(number) {
+    let arr = []
+    if (typeof number === "number") {
+      let a = new xQuery.fn.init()
+      arr.push(this[number - 1])
+      return xQuery.fn.merge(a, arr)
+    } else if (typeof number === "string") {
+      let a = new xQuery.fn.init()
+      arr.push(...this.parseTextChild(number))
+      return xQuery.fn.merge(a, arr)
+    }
+  },
+  getEventMap(a = false) {
+    if (a) {
+      return {
+      }
+    }
+    return {
+      click: "onClick",
+      start: "onStart",
+    }
+  },
+  on(type, callback = () => { }) {
+    let num = this.getObjNum(this)
+    let map = Script.eventTypeMap
+    let str = ""
+    if (!(type in map)) { return false }
+    for (let i in num) {
+      str = map[type] + "(){ let func = " + callback.toString() + ";func.bind(this)()}"
+      let c_name = `xQuery_${type}`
+      const obj = new Function(`return new class ${c_name} {${str}}`).bind(this)()
+      // 先移除同名事件
+      for (let event in this[i].script) {
+        const name = this[i].script[event].constructor.name
+        if (name === c_name) {
+          delete this[i].script[event]
+          if (typeof obj.update === 'function') {
+            this[i].script.parent.updaters?.remove(obj)
+          }
+          for (let sc in this[i].script.instances) {
+            const iobj = this[i].script.instances[sc]
+            const iname = iobj.constructor.name
+            if (iname === c_name) {
+              if (typeof iobj.update === 'function') {
+                this[i].script.parent.updaters?.remove(obj)
+              }
+              iobj.onScriptRemove?.(this[i].script.parent)
+              this[i].script.instances.remove(iobj)
+            }
+          }
+        }
+      }
+      // 添加事件
+      this[i].script.add(obj)
+    }
+  },
+  off(type) {
+    let num = this.getObjNum(this)
+    let map = Script.eventTypeMap
+    let str = ""
+    if (!(type in map)) { return false }
+    for (let i in num) {
+      let c_name = `xQuery_${type}`
+      for (let event in this[i].script) {
+        const name = this[i].script[event].constructor.name
+        if (name === c_name) {
+          delete this[i].script[event]
+          for (let sc in this[i].script.instances) {
+            const iobj = this[i].script.instances[sc]
+            const iname = iobj.constructor.name
+            if (iname === c_name) {
+              if (typeof iobj.update === 'function') {
+                this[i].script.parent.updaters?.remove(obj)
+              }
+              iobj.onScriptRemove?.(this[i].script.parent)
+              this[i].script.instances.remove(iobj)
+            }
+          }
         }
       }
     }
-  }
+  },
 }
 export default function () {
   xQuery.fn.init.prototype = xQuery.fn;
